a8b8c10e5a3fbd5692c9c021a4d7dbf9dbbd5ba2
Lorrane Idevic Gonçalves <lorraneidevic@MacBook-Pro-de-Lorrane.local>
2018-04-25 16:32:53 -0300

    printig FizzBuzz respecting the rules

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main (String[] args) {
+        int maxNumbers = 100;
+
+        for (int i = 1; i <= maxNumbers; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
